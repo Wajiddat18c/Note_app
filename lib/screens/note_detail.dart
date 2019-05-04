f5aa2e91a94afea2a0ba2355b25fddfa6a225fc8
Wajiddat18c <waji0003@stud.kea.dk>
2019-05-04 20:27:19 +0200

    added icon/color methods

--- lib/screens/note_detail.dart
+++ lib/screens/note_detail.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:note_app/models/note.dart';
+import 'package:note_app/utils/database_helper.dart';
+import 'package:intl/intl.dart';
 
 class NoteDetail extends StatefulWidget {
 
--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/screens/note_detail.dart';
+import "dart:async";
+import "package:note_app/models/note.dart";
+import 'package:note_app/utils/database_helper.dart';
+import 'package:sqflite/sqflite.dart';
 
 class NoteList extends StatefulWidget {
+//  int count = 0;
+
   @override
   _NoteListState createState() => _NoteListState();
 }
 
 class _NoteListState extends State<NoteList> {
   int count = 0;
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  List<Note> noteList;
 
   @override
   Widget build(BuildContext context) {
+    if (noteList == null) {
+      noteList = List<Note>();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Notes"),
       ),
       body: getNoteListView(),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-         debugPrint("FAB clicked");
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("FAB clicked");
 
-         navigateToDetail("Tilføj Notat");
-      },
+          navigateToDetail("Tilføj Notat");
+        },
         tooltip: "Tilføj ny Notat",
-        
         child: Icon(Icons.add),
-
       ),
     );
   }
@@ -62,12 +73,56 @@ class _NoteListState extends State<NoteList> {
     );
   }
 
+  //Returns the priority color
+
+  Color getPriorityColor(int priority) {
+    switch (priority) {
+      case 1:
+        return Colors.red;
+        break;
+      case 2:
+        return Colors.yellow;
+        break;
+
+      default:
+        return Colors.yellow;
+    }
+  }
+
+  Icon getPriorityIcon(int priority) {
+    switch (priority) {
+      case 1:
+        return Icon(Icons.play_arrow);
+        break;
+      case 2:
+        return Icon(Icons.keyboard_arrow_right);
+
+      default:
+        return Icon(Icons.keyboard_arrow_right);
+    }
+  }
+
+  void _delete(BuildContext context, Note note)async{
+
+    int result = await databaseHelper.deleteNote(note.id);
+    if(result != 0){
+      _showSnackBar(context, "Noten er slettet");
+
+    }
+  }
+
+  void _showSnackBar(BuildContext context, String message){
+
+    final snackBar = SnackBar(content: Text(message));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  //Returns the priority icon
 
   //navigate between screens
-  void navigateToDetail(String titel){
-    Navigator.push(context, MaterialPageRoute(builder: (context){
+  void navigateToDetail(String titel) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
       return NoteDetail(titel);
     }));
-
   }
 }
